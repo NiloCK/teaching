7529d256e4785814e5f8c57bcb5a2b2e91ea26fe
nilock <colinkennedy@hotmail.com>
2017-05-28 21:19:06 -0600

    Add a basic implementation for a Grader class

--- countby/src/appUtilities/Grader.ts
+++ countby/src/appUtilities/Grader.ts
@@ -2,6 +2,69 @@ import Recorder from './Recorder'
 
 export default class Grader {
     static Grade(qType: string, a: number, b: number) {
-        return Math.random();
+        const records = Recorder.getRecord();
+
+        let qCount: number = 0;
+        let index: number = records.length - 1;
+
+        let scorables = new Array<any>();
+
+        while (qCount < 6 && index >= 0) {
+            if (records[index].q === qType &&
+                records[index].a === a &&
+                records[index].b === b &&
+                records[index].correct) {
+                scorables.push(records[index]);
+            }
+            index--;
+        }
+
+        let totalAttempts = 0;
+        let totalTime = 0;
+
+        scorables.forEach((answer) => {
+            totalAttempts += answer.attempts;
+            totalTime += answer.time;
+        })
+
+
+        return new Grade(
+            (scorables.length / totalAttempts),
+            (totalTime / scorables.length)
+        );
+    }
+}
+
+export class Grade {
+    isValid: boolean;
+    score: number;
+    averageTime: number;
+
+    constructor(score: number, time: number) {
+        this.score = score;
+        this.averageTime = time;
+
+        if (isNaN(score) || isNaN(time)) {
+            this.isValid = false;
+        } else {
+            this.isValid = true;
+        }
+    }
+    toString(): string {
+        if (this.isValid) {
+            return this.score.toFixed(2).toString();
+        } else {
+            return "";
+        }
+    }
+    getRGB(): string {
+        if (this.isValid) {
+            return "rgb(" +
+                Math.floor(255 * Math.min(1, 2 - 2 * this.score)) + "," +   // red
+                Math.floor(255 * Math.min(1, 2 * this.score)) +             // green
+                ", 0)"; // blue
+        } else {
+            return "white";
+        }
     }
 }
