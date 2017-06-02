32ddc116ec6fb8c6371cfe0d1df0f317f4b92f77
nilock <colinkennedy@hotmail.com>
2017-06-02 16:24:29 -0600

    Refactor localStorage to pouch/couch w/ cloudant

--- countby/src/appUtilities/Recorder.ts
+++ countby/src/appUtilities/Recorder.ts
@@ -1,24 +1,55 @@
+import * as pouch from 'pouchdb-browser'
+
 const key = 'record';
+const localDB: PouchDB.Database<PouchDB.Core.Encodable> = new pouch(key);
+const remoteDB: PouchDB.Database<PouchDB.Core.Encodable> = new pouch(
+    'https://' +
+    'nilock.cloudant.com/' +
+    key
+);
+
+localDB.sync(remoteDB, {
+    live: true,
+    retry: true
+})
 
 export default class Recorder {
-    static getRecord(): Array<any> {
-        let record = JSON.parse(
-            window.localStorage.getItem(key)
-        );
+    private static localDB: PouchDB.Database<PouchDB.Core.Encodable> = localDB;
+    private static remoteDB: PouchDB.Database<PouchDB.Core.Encodable> = remoteDB;
 
-        if (record === null) {
-            return [];
-        } else {
-            return record;
-        }
+    private static record: Array<any> = new Array<any>();
+
+    static init(): void {
+        console.log("Recorder initializing");
+        Recorder.localDB.allDocs({
+            include_docs: true
+        }).then(
+            (docs: PouchDB.Core.AllDocsResponse<PouchDB.Core.Encodable>) => {
+                docs.rows.forEach((row) => {
+                    // console.log("Adding " + row + " to in-memory records.");
+                    Recorder.record.push(row.doc);
+                });
+            }
+            ).catch((err) => {
+                console.log(err);
+            });
+    }
+
+    static getRecord(): Array<any> {
+        return Recorder.record;
     }
 
     static addRecord(record: Object) {
-        let records = Recorder.getRecord();
-        // console.log('Records: ' + records.toString());
+        console.log("Adding a record...");
 
-        records.push(record);
+        record._id = new Date().toJSON();
 
-        window.localStorage.setItem(key, JSON.stringify(records));
+        // add record to in-memory records
+        Recorder.record.push(record);
+        // add record to db - syncs with cloudant
+        Recorder.localDB.put(record);
     }
+
+
+
 }
