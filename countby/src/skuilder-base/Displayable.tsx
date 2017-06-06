ad51c6388fbc7572a1973a4d19db9bf35f0ae2d3
nilock <colinkennedy@hotmail.com>
2017-06-06 12:54:21 -0600

    A start at skuilder-base

---
+++ countby/src/skuilder-base/Displayable.tsx
@@ -0,0 +1,75 @@
+import * as RX from 'reactxp';
+import * as Moment from 'moment';
+
+abstract class Displayable<Props, State> extends RX.Component<RX.CommonProps, null> {
+    startTime: Moment.Moment;
+
+    abstract render(): JSX.Element;
+
+    init(): void {
+        this.startTime = Moment();
+    }
+    timeSinceStart(): number {
+        let now = Moment();
+        return now.diff(this.startTime);
+    }
+
+    constructor(props: RX.CommonProps) {
+        super(props);
+    }
+}
+
+export abstract class Question<P, S> extends Displayable<QuestionProps, null> {
+    attempts: number;
+    static readonly staticThing: number = 5; //?
+
+    init(): void {
+        super.init();
+        this.attempts = 0;
+    }
+
+    readonly componentDidMount = (): void => {
+        this.init();
+    }
+
+    abstract isCorrect(): boolean;
+
+    constructor(props: RX.CommonProps) {
+        super(props);
+    }
+}
+
+export interface QuestionProps extends RX.CommonProps {
+    onanswer: Function
+}
+
+class SubtractionProb extends Question<RX.CommonProps, null> {
+    /**
+     *
+     */
+    constructor() {
+        super();
+
+    }
+    render() { return (<div></div>); }
+}
+
+// interface QuestionProps {
+//     props: Array<Property>
+// }
+
+interface Property {
+
+}
+
+/**
+ * The record of a completed question.
+ */
+interface QuestionRecord {
+    q: string, // the ClassName of the question (?)
+    props: QuestionProps,
+    answer: any,
+    isCorrect: boolean,
+    attempts: number,
+    time: number // milliseconds
+}
