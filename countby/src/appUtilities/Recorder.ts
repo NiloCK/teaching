5be3cac9d953bfeee98643cc5830fc9c3826c1e6
nilock <colinkennedy@hotmail.com>
2017-06-05 20:47:59 -0600

    Work toward a user login / personal data storage

--- countby/src/appUtilities/Recorder.ts
+++ countby/src/appUtilities/Recorder.ts
@@ -1,17 +1,34 @@
 import * as pouch from 'pouchdb-browser'
+// import * as auth from 'pouchdb-authentication'
+
+pouch.plugin(require('pouchdb-authentication'))
+
+const auth = require('pouchdb-authentication');
 
 const key = 'record';
 const localDB: PouchDB.Database<PouchDB.Core.Encodable> = new pouch(key);
 const remoteDB: PouchDB.Database<PouchDB.Core.Encodable> = new pouch(
     'https://' +
     'nilock.cloudant.com/' +
-    key
+    key, {
+        skip_setup: true
+    }
 );
 
 localDB.sync(remoteDB, {
     live: true,
     retry: true
 })
+
+
+// works - need a login/register page. Still waiting on per-user dbs
+// remoteDB.signup('newUser', 'password', function (err, resp) {
+//     if (err) {
+//         console.log("Signup error:" + err.name);
+//     } else {
+//         console.log("Signup success!" + resp.name);
+//     }
+// })
 
 export default class Recorder {
     private static localDB: PouchDB.Database<PouchDB.Core.Encodable> = localDB;
