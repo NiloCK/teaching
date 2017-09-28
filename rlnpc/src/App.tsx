40014d50b8a4435eecfc2e1647ff6125a6789291
nilock <colinkennedy@hotmail.com>
2017-09-27 23:04:33 -0600

    Inital work on Turtle Programming Club App
    create-react-app + monaco editor (the editor from vscode)

--- rlnpc/src/App.tsx
+++ rlnpc/src/App.tsx
@@ -1,10 +1,26 @@
 import * as React from 'react';
 import './App.css';
+import MonacoEditor from 'react-monaco-editor';
 
 const logo = require('./logo.svg');
 
 class App extends React.Component {
+  editor: any;
+
+  handleEditorDidMount = (editor: any) => {
+    this.editor = editor;
+    console.log(
+      (this.editor as MonacoEditor)
+    );
+    editor.height = (document.getElementsByClassName("App")[0] as HTMLDivElement).clientHeight -
+    ( (document.getElementsByClassName("App-header")[0] as HTMLDivElement).clientHeight + 
+    (document.getElementsByClassName("App-intro")[0] as HTMLDivElement).clientHeight )
+  }
+
   render() {
+    let editorWidth: number = window.innerWidth / 2;
+    let editorHeight: number = window.innerHeight;
+
     return (
       <div className="App">
         <div className="App-header">
@@ -14,6 +30,15 @@ class App extends React.Component {
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <MonacoEditor
+          width={editorWidth}
+          height={editorHeight}
+          language="typescript"
+          theme="vs-dark"
+          value=""
+          text-align="left"
+          editorDidMount={this.handleEditorDidMount}
+        />
       </div>
     );
   }
