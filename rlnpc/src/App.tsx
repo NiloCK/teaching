a4f261db20df3b204f7c713c1b8950cbc93f6dec
nilock <colinkennedy@hotmail.com>
2017-10-01 21:54:48 -0600

    Work toward turtle drawing

--- rlnpc/src/App.tsx
+++ rlnpc/src/App.tsx
@@ -1,20 +1,35 @@
 import * as React from 'react';
 import './App.css';
 import MonacoEditor from 'react-monaco-editor';
+import { Play } from './components/Buttons'
+import Turtle from './turtle/turtle';
 
 const logo = require('./logo.svg');
 
 class App extends React.Component {
-  editor: any;
+  editor: monaco.editor.ICodeEditor;
 
   handleEditorDidMount = (editor: any) => {
+    // for providing type info to monaco editor
+    // https://stackoverflow.com/questions/43037243/provide-type-hints-to-monaco-editor
+    monaco.languages.typescript.typescriptDefaults.addExtraLib(``
+      , './turtle/turtle.ts');
+
     this.editor = editor;
-    console.log(
-      (this.editor as MonacoEditor)
-    );
-    editor.height = (document.getElementsByClassName("App")[0] as HTMLDivElement).clientHeight -
-      ((document.getElementsByClassName("App-header")[0] as HTMLDivElement).clientHeight +
-        (document.getElementsByClassName("App-intro")[0] as HTMLDivElement).clientHeight)
+  }
+
+  runEditorCode = () => {
+    this.clearTurtleCanvas();
+    eval(this.editor.getValue());
+  }
+  clearTurtleCanvas = () => {
+    let cvs = document.getElementById('turtleCanvas') as HTMLCanvasElement;
+    let ctx = cvs.getContext('2d') as CanvasRenderingContext2D;
+    ctx.clearRect(0, 0, cvs.height, cvs.width);
+  }
+
+  getEditorCode = () => {
+    return this.editor.getValue();
   }
 
   render() {
@@ -24,23 +39,33 @@ class App extends React.Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to RLNPC</h2>
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <h2>Welcome to the RLNPC</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <div className="App-intro">
+          <Play click={this.runEditorCode} />
+        </div>
         <div id="EditorAndCanvas">
           <MonacoEditor
             width={editorWidth}
             height={editorHeight}
             language="typescript"
+            //onChange={this.runEditorCode}
             theme="vs-dark"
-            value=""
+            value="// Type your code in here."
+            options={{
+              codeLens: false,
+              lineNumbersMinChars: 3,
+              /* minimap: { enabled: false }, */
+              /* showFoldingControls: "always", */
+              snippetSuggestions: 'none',
+              wordBasedSuggestions: false
+            }}
             text-align="left"
             editorDidMount={this.handleEditorDidMount}
           />
-          <canvas id="canvas"
+          <canvas
+            id="turtleCanvas"
             width={editorWidth}
             height={editorHeight}
           />
