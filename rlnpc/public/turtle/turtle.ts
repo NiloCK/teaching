c8aac22d06702f8e88d7a7f240f99ee24841034f
nilock <colinkennedy@hotmail.com>
2017-10-08 12:54:03 -0600

    Work toward drawing management

--- rlnpc/public/turtle/turtle.ts
+++ rlnpc/public/turtle/turtle.ts
@@ -1,26 +1,109 @@
 // class Animation {}
+class Pen {
+    color: string;
+    width: number;
+}
 
+class Queue<T>{
+    private queue: Array<T> = new Array<T>();
+
+    enqueue(element: T): void {
+        this.queue.push(element);
+    }
+    dequeue(): T | null {
+        let ret = this.queue.shift();
+
+        if (ret) {
+            return ret;
+        }
+        else {
+            return null;
+        }
+    }
+
+}
+
+class CanvasStroke {
+    initX: number;
+    initY: number;
+
+    finalX: number;
+    finalY: number;
+
+    pen: Pen;
+}
+
+class AnimationManager {
+    frames: Queue<Array<CanvasStroke>>
+    ctx: CanvasRenderingContext2D
+
+    constructor() {
+        this.frames = new Queue<Array<CanvasStroke>>();
+    }
+
+    setContext(ctx: CanvasRenderingContext2D): void {
+        this.ctx = ctx;
+    }
+
+    animate = () => {
+        let strokes = this.frames.dequeue();
+        if (strokes != null) {
+            strokes.forEach(stroke => {
+                this.ctx.strokeStyle = stroke.pen.color;
+                this.ctx.lineWidth = stroke.pen.width;
+                this.ctx.lineCap = "square";
+
+                this.ctx.beginPath();
+                this.ctx.moveTo(stroke.initX, stroke.initY);
+                this.ctx.lineTo(stroke.finalX, stroke.finalY);
+                this.ctx.stroke();
+            });
+        }
+        requestAnimationFrame(this.animate);
+    }
+}
 
 class Turtle {
-    x: number;
-    y: number;
+    private static AnimationManager: AnimationManager = new AnimationManager();
+    private static drawTurtles: boolean = true;
+    static hide = () => {
+        Turtle.drawTurtles = false;
+    }
+    static show = () => {
+        Turtle.drawTurtles = true;
+    }
+
+    /**
+     * The x coordinate of the Turtle
+     */
+    private x: number;
+    /**
+     * The y coordinate of the Turtle
+     */
+    private y: number;
     /**
      * Angle, in radians, that the turtle is facing
      */
     private angle: number = (Math.PI / 2); // radians
 
     private drawing: boolean = true;
-    visible: boolean = true;
+    /**
+     * The speed of the turtle. Set to 0 for the turtle to draw instantly.
+     */
     speed: number = 10; // *10 px/second? 0 == inf
-
     ctx: CanvasRenderingContext2D;
 
     constructor(x?: number, y?: number, canvas?: HTMLCanvasElement, ) {
+
         if (canvas) {
             this.ctx = <CanvasRenderingContext2D>canvas.getContext("2d");
         } else {
             canvas = <HTMLCanvasElement>document.getElementById('turtleCanvas');
             this.ctx = <CanvasRenderingContext2D>canvas.getContext("2d");
+        }
+
+        if (Turtle.AnimationManager.ctx == null) {
+            Turtle.AnimationManager.setContext(this.ctx);
         }
 
         this.x = (x != undefined) ? x : Math.round(canvas.width / 2);
@@ -29,66 +112,127 @@ class Turtle {
 
 
     /**
-     * Creates a new Turtle at the specified coordinates, or with the same
+     * Creates a new turtle at the specified coordinates, or with the same
      * location / orientation of the parent if no coordinates are given.
      * @param x 
      * @param y 
      * @param angle 
      */
-    newTurtle(x?: number, y?: number, angle?: number): Turtle {
+    newTurtle(x?: number, y?: number, speed?: number, angle?: number): Turtle {
         let babyTurtle = new Turtle(
             x ? x : this.x,
             y ? y : this.y,
             this.ctx.canvas);
 
+        babyTurtle.speed = speed ? speed : this.speed;
         babyTurtle.angle = angle ? angle : this.angle;
 
         return babyTurtle;
     }
 
+    /**
+     * Creates a 'family' of new turtles at the same location and angle
+     * of the parent turtle.
+     * @param n The number of turtles to produce
+     */
+    newTurtles(n: number): Array<Turtle> {
+        let ret: Array<Turtle> = [];
+
+        for (let i = 0; i < n; i++) {
+            ret.push(this.newTurtle());
+        }
+
+        return ret;
+    }
+
+    /**
+     * Lifts the turtle's drawing pen.
+     */
     penUp() {
         this.drawing = false;
     }
+    /**
+     * Lowers the turtle's drawing pen.
+     */
     penDown() {
         this.drawing = true;
     }
-
+    /**
+     * @param distance The distance, in pixels, to move.
+     */
     move(distance: number): void {
-        console.log("moving a turtle...");
+        // console.log("moving a turtle...");
         let dx = Math.cos(this.angle) * distance;
         let dy = Math.sin(this.angle) * distance;
 
         if (this.drawing) {
-            this.draw(dx, dy, this.moveTime(distance));
-        } else {
-            this.x += dx;
-            this.y += dy;
+            // this.draw(dx, dy, this.moveTime(distance));
+            let strokes = this.getStrokes(
+                this.x,
+                this.y,
+                this.x + dx,
+                this.y + dy,
+                this.moveAnimationFrameCount(distance));
+            strokes.forEach(stroke => {
+                Turtle.AnimationManager.frames.enqueue([stroke]);
+            });
+
+            Turtle.AnimationManager.animate();
         }
+
+        this.x += dx;
+        this.y += dy;
+
     }
 
-    private draw(dx: number, dy: number, time: number): void {
-        console.log("drawing a line..."); // works, is called. How to animate properly?
-        this.ctx.beginPath();
-        this.ctx.moveTo(this.x, this.y);
+    private getStrokes(
+        initX: number,
+        initY: number,
+        finalX: number,
+        finalY: number,
+        frameCount: number
+    ): Array<CanvasStroke> {
+        let dx = (finalX - initX) / frameCount;
+        let dy = (finalY - initY) / frameCount;
 
-        console.log(`
-            dx: ${dx}
-            dy: ${dy}`);
+        let runningX = initX;
+        let runningY = initY;
 
-        //todo speed is being used backwards somehow.
-        //todo speed = 0 is resulting in no drawing
-        let stepX: number = (dx > 1) ? (time / 30) * dx : dx;
-        let stepY: number = (dy > 1) ? (time / 30) * dy : dy;
+        let ret = new Array<CanvasStroke>();
 
-        this.ctx.lineTo(this.x + stepX, this.y + stepY);
-        this.ctx.stroke();
+        let count = 1;
 
-        this.x += stepX;
-        this.y += stepY;
-        if (dx > 0 || dy > 0) {
-            window.requestAnimationFrame(this.draw.bind(this, dx - stepX, dy - stepY, time));
+        while (count < frameCount) {
+            ret.push({
+                initX: runningX,
+                initY: runningY,
+                finalX: runningX + dx,
+                finalY: runningY + dy,
+                pen: {
+                    color: 'black',
+                    width: 2
+                }
+            });
+
+            count++;
+            runningX += dx;
+            runningY += dy;
         }
+
+        ret.push({
+            initX: runningX - dx,
+            initY: runningY - dy,
+            finalX: finalX,
+            finalY: finalY,
+            pen: {
+                color: 'black',
+                width: 2
+            }
+        })
+
+        return ret;
     }
+
     /**
      * Turn the turtle to the left.
      * @param fraction The fraction of a whole turn that the turtle will turn
@@ -108,11 +252,6 @@ class Turtle {
         this.normalizeAngle();
     }
 
-    hide(): void {
-        this.visible = false;
-    }
-
-
     /**
      * Returns the length of time in seconds for a 'move' command
      * @param distance The distance in pixels of the movement
@@ -121,20 +260,28 @@ class Turtle {
         if (this.speed === 0) {
             return 0;
         } else {
-            return distance / (10 * this.speed);
+            let ret = distance / (10 * this.speed)
+            console.log(`This line will take: ${ret} seconds`);
+            return ret;
         }
     }
 
     /**
-     * 
+     * Returns the number of steps that a turtle's move operation will take,
+     * depending on the distance and the turtle's current speed.
      * @param distance The distance in pixels of the movement
      */
-    private moveSteps(distance: number): number {
-        return this.moveTime(distance) / 0.016; // requestAnimationFrame
+    private moveAnimationFrameCount(distance: number): number {
+        let time: number = this.moveTime(distance);
+        if (time === 0) {
+            return 1; // do the animation in one step
+        } else {
+            return Math.floor(this.moveTime(distance) / 0.016); // requestAnimationFrame
+        }
     }
 
     private normalizeAngle(): void {
-        this.moveSteps(0);
+        //this.moveAnimationFrameCount(0); // should I animate the turning Turtle?
 
         while (this.angle >= 2 * Math.PI) {
             this.angle -= 2 * Math.PI;
